992930bfb7175173a5739f148922a48ff6e48225
mabel-idm <mabel.gutierrez@idmtechnology.com.pe>
2025-07-30 11:56:07 -0500

    Agregar práctica de booleans

---
+++ D3/practica/booleans.py
@@ -0,0 +1,22 @@
+"""
+BOOLEANS
+"""
+
+# PRÁCTICA 1
+# Realiza una comparación que arroje como resultado un booleano y almacena el resultado en una variable llamada prueba.
+
+prueba = 25 > 50 
+
+print(prueba)
+
+#...........................................................................................
+# PRÁCTICA 2
+# Verifica si 17834/34 es mayor que 87*56 y muestra el resultado en pantalla utilizando print().
+
+print ((17834/34) > (87*56))
+
+#...........................................................................................
+# PRÁCTICA 3
+# Verifica si la raíz cuadrada de 25 es igual a 5 y muestra el resultado en pantalla utilizando print().
+
+print(25**0.5 == 5)
