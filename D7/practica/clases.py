42de9bd5a7082c08b52b02d6eb441de3ab2a1624
mabel-idm <mabel.gutierrez@idmtechnology.com.pe>
2025-08-29 11:02:49 -0500

    Día 07 - Clases

---
+++ D7/practica/clases.py
@@ -0,0 +1,33 @@
+""" Clases
+            class NombreDeLaClase:
+"""
+
+# PRÁCTICA 1
+# Crea una clase llamada Personaje y a continuación, crea un objeto a partir de ella, por ejemplo: harry_potter
+
+class Personaje:
+    pass
+
+harry_potter = Personaje()     # Aquí se crea un objeto (instancia) a partir de la clase Personaje
+
+#...........................................................................................
+# PRÁCTICA 2
+# Crea una clase llamada Dinosaurio, y tres instancias a partir de ella: velociraptor, tiranosaurio_rex y braquiosaurio .
+
+class Dinosaurio:
+    pass
+
+velociraptor = Dinosaurio()        # Al crear el objeto (instancia) se llama a la clase junto a unos paréntesis
+tiranosaurio_rex = Dinosaurio()
+braquiosaurio = Dinosaurio()
+
+#...........................................................................................
+# PRÁCTICA 3
+# Crea una clase llamada PlataformaStreaming y crea los siguientes objetos a partir de ella: netflix, hbo_max, amazon_prime_video
+
+class PlataformaStreaming:
+    pass
+
+netflix = PlataformaStreaming()
+hbo_max = PlataformaStreaming()
+amazon_prime_video = PlataformaStreaming()
