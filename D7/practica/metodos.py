6fc668ad7974cbe6f3bcb83979f8abf710ce9fc6
mabel-idm <mabel.gutierrez@idmtechnology.com.pe>
2025-09-01 12:15:42 -0500

    Día 07 - Métodos

---
+++ D7/practica/metodos.py
@@ -0,0 +1,39 @@
+""" Métodos """
+
+# PRÁCTICA 1
+# Crea una clase Perro. Dicho perro debe poder ladrar.
+# Crea el método ladrar() y ejecútalo en una instancia de Perro. Cada vez que ladre, debe mostrarse en pantalla "Guau!".
+
+class Perro:
+
+    def ladrar(self):   # Método propio
+        print('¡Guau!')
+
+toby = Perro()      # Crear instancia
+toby.ladrar()       # Ejecutar método
+   
+#...........................................................................................
+# PRÁCTICA 2
+# Crea una clase llamada Mago, y crea un método llamado lanzar_hechizo() (deberá imprimir "¡Abracadabra!").
+# Crea una instancia de Mago en el objeto merlin, y haz que lance un hechizo.
+
+class Mago:
+
+    def lanzar_hechizo(self):
+        print('¡Abracadabra!')
+
+merlin = Mago()
+merlin.lanzar_hechizo()
+
+#...........................................................................................
+# PRÁCTICA 3
+# Crea una instancia de la clase Alarma, que tenga un método llamado postergar(cantidad_minutos). El método debe imprimir en pantalla el mensaje
+# "La alarma ha sido pospuesta {cantidad_minutos} minutos"
+
+class Alarma:
+
+    def postergar (self, cantidad_minutos):
+        print(f'La alarma ha sido pospuesta {cantidad_minutos} minutos')
+
+alarma_uno = Alarma()
+alarma_uno.postergar(5)
