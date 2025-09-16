77baae71051cecdade63a9a5f2f6eef542509c75
mabel-idm <mabel.gutierrez@idmtechnology.com.pe>
2025-09-16 12:35:39 -0500

    Día 09 - Module Math

---
+++ D9/practica/module-math.py
@@ -0,0 +1,26 @@
+""" MÓDULO MATH """
+
+import math
+
+# PRÁCTICA 1
+# Obtén el logaritmo base 10 del número 25, y almacena el resultado en la variable resultado.
+# Puedes utilizar el método math.log10()
+
+resultado = math.log(25, 10)
+print(resultado)
+
+
+#...........................................................................................
+# PRÁCTICA 2
+# Obten la raíz cuadrada de pi con la constante math.pi y el método math.sqrt() . Almacena el resultado obtenido en la variable resultado.
+
+resultado = math.sqrt(math.pi)
+print(resultado)
+
+#...........................................................................................
+# PRÁCTICA 3
+# Encuentra el factorial de 7 y almacena el resultado en la variable resultado.
+# El método a utilizar es factorial()
+
+resultado = math.factorial(7)
+print(resultado)
